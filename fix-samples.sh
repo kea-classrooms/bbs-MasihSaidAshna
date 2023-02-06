e4d48e16438c17ca3ed4cfb4f66340674e0c4b8c
MasihSaidAshna <113370064+MasihSaidAshna@users.noreply.github.com>
2023-02-06 12:14:53 +0000

    Issues done

---
+++ fix-samples.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mv ./samples/animals/countries ./samples
+mv ./samples/countries/movies ./samples
+
+mv ./samples/animals/Monkies.txt ./samples/animals/Monkeys.txt
+
+mv ./samples/movies/Germany.txt ./samples/countries
+
+mv ./samples/countries/.apes.txt ./samples/animals
+
